4a0320d3a997a01136b766f16953d497ca6eb0eb
ChiaYen <kcygamil@gmail.com>
2019-03-17 13:42:00 +0800

    add custom board setting to NanoPi NEO4

--- include/configs/nanopi_neo4.h
+++ include/configs/nanopi_neo4.h
@@ -3,8 +3,6 @@
 
 #include <configs/rk3399_common.h>
 
-#pragma message "NanoPi NEO4"
-
 #ifndef CONFIG_SPL_BUILD
 #undef CONFIG_BOOTCOMMAND
 #define CONFIG_BOOTCOMMAND \
@@ -13,9 +11,19 @@
 #endif
 
 #define CONFIG_MMC_SDHCI_SDMA
+#define CONFIG_SYS_MMC_ENV_DEV 0
+
+#define SDRAM_BANK_SIZE			(2UL << 30)
+#define CONFIG_MISC_INIT_R
+#define CONFIG_SERIAL_TAG
+#define CONFIG_ENV_OVERWRITE
 
 #define CONFIG_BMP_16BPP
 #define CONFIG_BMP_24BPP
 #define CONFIG_BMP_32BPP
+
+#define ROCKCHIP_DEVICE_SETTINGS \
+		"stdout=serial,vidconsole\0" \
+		"stderr=serial,vidconsole\0"
 
 #endif
